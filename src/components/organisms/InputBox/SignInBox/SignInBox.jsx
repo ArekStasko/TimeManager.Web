ec0e7e7393e927698c2bf3350642f61c654d022f
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-02-18 17:13:21 +0100

    add login api to login component

--- src/components/organisms/InputBox/SignInBox/SignInBox.jsx
+++ src/components/organisms/InputBox/SignInBox/SignInBox.jsx
@@ -1,24 +1,60 @@
+import React from "react"
+import {connect} from "react-redux";
 import "./SignInBox.css"
 import Button from "../../../atoms/Button/Button";
+import {login} from "../../../../actions";
 
 
-const SignInBox = () => {
+class SignInBox extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: ""
+        };
+    };
 
-    const signInBox = (
-        <div className="wrapper">
-            <div className="input-wrapper">
-                <label>Username</label>
-                <input className="input" />
+    HandleSubmit(e){
+        e.preventDefault();
+        this.props.login(this.state.username, this.state.password);
+    }
+
+    render(){
+        return (
+            <div className="wrapper">
+                <div className="input-wrapper">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={this.state.username}
+                        onChange={e => this.setState({username: e.target.value})}
+                        className="input"
+                    />
+                </div>
+                <div className="input-wrapper">
+                    <label>Password</label>
+                    <input
+                        id="password"
+                        value={this.state.password}
+                        onChange={e => this.setState({password: e.target.value})}
+                        className="input"
+                        type="password"
+                    />
+                </div>
+                <Button text={"Sign In"} event={e => this.HandleSubmit(e)} />
             </div>
-            <div className="input-wrapper">
-                <label>Password</label>
-                <input  className="input" type="password"/>
-            </div>
-            <Button text={"Sign In"} />
-        </div>
-    );
-
-    return signInBox;
+        );
+    }
 }
 
-export  default  SignInBox;
+const mapStateToProps = ({flash, loading }) => ({
+    flash,
+    loading,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    login: (username, password) => dispatch(login(username, password)),
+});
+
+export  default  connect(mapStateToProps, mapDispatchToProps)(SignInBox);
