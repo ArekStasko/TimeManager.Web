72ea70ccc35f0d7a7decca0cbd584c3a90897000
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-04-14 20:12:15 +0200

    Make calendar layout better

--- src/Layouts/MonthLayout.jsx
+++ src/Layouts/MonthLayout.jsx
@@ -1,4 +1,4 @@
-import {Chip} from "@mui/material";
+import {Chip, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
 import {weekDays, GetMonthLayout} from "../services/CalendarService";
 import {Day} from "../components/Day";
 
@@ -7,23 +7,33 @@ export const MonthLayout = () => {
     const days = GetMonthLayout();
     return(
         <div className="monthLayout">
-            <div className="monthLayout__dayBar">
-                {weekDays.map(name => (
-                    <div className="monthLayout__dayBar--dayName">
-                        <Chip label={name} variant="outlined" color="primary" sx={{color: "#E8EAF6"}} />
-                    </div>
-                ))
-                }
-            </div>
-            <div className="monthLayout__body">
-                {
-                    days.map(component => (
-                        <>
-                            {component}
-                        </>
-                    ))
-                }
-            </div>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {weekDays.map(name => (
+                                <TableCell align="center" className="monthLayout__dayBar--dayName">
+                                    <Chip label={name} variant="outlined" color="primary" sx={{color: "#E8EAF6"}} />
+                                </TableCell>
+                            ))
+                            }
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            days.map(week => (
+                                <TableRow >
+                                    {
+                                        week.map(days => (
+                                            <TableCell>
+                                                {days}
+                                            </TableCell>
+                                        ))
+                                    }
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
         </div>
     )
 }
--- src/services/CalendarService.js
+++ src/services/CalendarService.js
@@ -3,15 +3,29 @@ import {Day} from "../components/Day";
 
 export const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
+
+//WARNING -- This method has spaghetti code and it is dirty - i know - i will clean in later ( yes i will ;D )
 export const GetMonthLayout = () => {
         const months = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
         const data = [];
         const date = new Date();
 
-        for(let i = 0; i<months[date.getMonth()]; i++){
-            data.push(<Day current={i === date.getDay()} />)
-        }
+        let leftDays = months[date.getMonth()]%7;
 
+        for(let i = 0; i < 4; i++){
+                let weekDays = []
+                for(let i = 0; i<7; i++){
+                        weekDays.push(<Day current={i === date.getDay()} />)
+                }
+                data.push(weekDays);
+        }
+        let leftDaysArr = [];
+        for(let i = 0; i < leftDays; i++){
+                leftDaysArr.push(<Day current={i === date.getDay()} />)
+        }
+        data.push(leftDaysArr);
+
+        console.log(data);
         return data;
 }
