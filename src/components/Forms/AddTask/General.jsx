60a270fca6a664b7794f233c00f7e0b7a6284e38
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-04-28 17:57:05 +0200

    Add general informations form

--- src/components/Forms/AddTask/General.jsx
+++ src/components/Forms/AddTask/General.jsx
@@ -1,10 +1,23 @@
+import {FormControl, IconButton, TextField, Tooltip, Typography} from "@mui/material";
 
 
 export const General = () => {
 
     return(
-        <div>
-            General
+        <div className="general">
+            <Typography>
+                Provide General Information's about your Task
+            </Typography>
+            <FormControl fullWidth className="general__form">
+                <TextField label="Title" variant="filled" margin="dense"/>
+                <TextField
+                    margin="dense"
+                    label="Description"
+                    multiline
+                    rows={9}
+                    variant="filled"
+                />
+            </FormControl>
         </div>
     )
 }
